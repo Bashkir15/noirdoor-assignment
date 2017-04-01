536768f2804b4486103c70440920c04358cf8900
Forrest Collins <Forest.d.collins@gmail.com>
2017-04-01 18:30:38 -0500

    Added a webpack config to bundle code, run xo-lint for me automatically, sets up a dev server, loads and extracts styles

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+	app: path.join(__dirname, 'app'),
+	dist: path.join(__dirname, 'dist'),
+	test: path.join(__dirname, 'test')
+};
+
+module.exports = {
+	devtool: 'cheap-module-eval-source-map',
+
+	entry: {
+		app: PATHS.app
+	},
+
+	output: {
+		filename: '[name].js',
+		chunkFilename: '[name].chunk.js',
+		publicPath: '/',
+		path: path.join(PATHS.dist)
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.(jsx|js)?$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+
+			{
+				test: /\.(jsx|js)?$/,
+				use: ['xo-loader'],
+				enforce: 'pre',
+				include: PATHS.app + '/**/*.+(jsx|js)'
+			},
+
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+				include: path.join(__dirname, 'test'),
+				exclude: /node_modules/
+			},
+
+			{
+				test: /\.json$/,
+				use: 'json-loader'
+			},
+
+			{
+				test: /\.sass$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			},
+
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader'
+			}
+		]
+	},
+
+	resolve: {
+		extensions: ['.js', '.jsx', '.json', '.sass', '.css']
+	},
+
+	devServer: {
+		historyApiFallback: true,
+		hot: true,
+		inline: true,
+		stats: 'errors-only',
+		host: 'localhost',
+		port: 3000,
+		proxy: {
+			"*": "http://localhost:8000"
+		}
+	},
+
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+		}),
+
+		new webpack.HotModuleReplacementPlugin(),
+
+		new ExtractTextPlugin('main.css')
+	]
+}
