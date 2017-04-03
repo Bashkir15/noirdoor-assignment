de61d81f50c6db7821f40d593cb052af149df450
Forrest Collins <Forest.d.collins@gmail.com>
2017-04-03 03:58:29 -0500

    Set up a very basic Schema using thinky for Artists and Songs

---
+++ server/models/artists.js
@@ -0,0 +1,24 @@
+import thinky from './db'
+
+const r = thinky.r;
+const type = thinky.type;
+
+const Artist = thinky.createModel('Artist', {
+	id: type.string(),
+	name: type.string(),
+	genres: type.array(),
+	bio: type.string(),
+	createdAt: type.date().default(r.now())
+});
+
+const Songs = thinky.createModel('Songs', {
+	id: type.string(),
+	name: type.string(),
+	album: type.string(),
+	location: type.string(),
+	createdAt: type.date().default(r.now()),
+	listens: type.number()
+});
+
+Artist.hasMany(Songs, "songs", "creatorId", "id");
+Songs.belongsTo(Artist, "artist", "creatorId");
